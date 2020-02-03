e5d7c87ec4b83e3cbcc541fddf93f60980baee6f
Mitchell <mover333@gmail.com>
2020-02-02 17:53:11 -0800

    fixed port and copy package

--- functions/serveReactHTTP/serveReact.ts
+++ functions/serveReactHTTP/serveReact.ts
@@ -1,6 +1,3 @@
-import * as dotenv from 'dotenv'
-dotenv.config()
-
 import * as path from 'path'
 import * as express from 'express'
 import * as webpack from 'webpack'
@@ -9,12 +6,7 @@ import * as webpack_hot_middleware from 'webpack-hot-middleware'
 import config from './webpack.config'
 
 const app = express()
-const port = process.env.PORT || 3000
 const dev = process.env.PROD === 'false'
-
-app.listen(port, () => {
-    console.log(`App is listening on port ${port}`)
-})
 
 app.get('/', (req: express.Request, res: express.Response) => {
     res.sendFile(path.resolve(__dirname, 'frontEnd', 'index.html'))
